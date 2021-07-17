b2f3ae250be3ef637152018728450cdd4c406bff
DivyaVignesh <divya.vignesh8994@gmail.com>
2021-07-17 08:35:21 +0530

    Added LoginView

---
+++ Shared/Utils/Contants.swift
@@ -0,0 +1,16 @@
+//
+//  Contants.swift
+//  E-commerce
+//
+//  Created by Divya on 17/07/21.
+//  Copyright © 2021 Divya. All rights reserved.
+//
+
+import Foundation
+
+struct Storyboard {
+    static let Loginstoryboard = "LoginStoryBoard"
+}
+struct StoryboardId {
+    static let LoginView = "LoginView"
+}
---
+++ Shared/Utils/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  E-commerce
+//
+//  Created by Divya on 16/07/21.
+//  Copyright © 2021 Divya. All rights reserved.
+//
+
+import Foundation
+
+
+extension String
+{
+    var isNotEmpty : Bool
+    {
+        return !isEmpty
+    }
+    
+}
